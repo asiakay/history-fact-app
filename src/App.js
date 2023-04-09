63c70a56480a1eafe63abd6caf4eaaf7a2db5651
Asia K <asialakaygrady@gmail.com>
2023-04-09 09:32:16 -0400

    added functionality to display fact by category

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from "./GlobalStyle";
 import Background from "./components/Background";
-import QuoteDisplay from './components/QuoteDisplay';
+import QuoteDisplay from '../src/components/QuoteDisplay';
 //import './style.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import styled from 'styled-components';
-
+import quotes from '../src/data/quotes.json';
 
 
 const AppMain = styled.main`
@@ -20,13 +20,32 @@ display: flex;
 `;
 
 function App() {
+      /* Add state variables to store the selected category and filtered quotes: */
+      const [selectedCategory, setSelectedCategory] = useState('');
+      const categories = Array.from(new Set(quotes.map((quote) => quote.category)));
+    
+      const handleCategoryChange = (event) => {
+        const value = event.target.value;
+        setSelectedCategory(value === '' ? 'all' : value);
+      };
+
+       const filteredQuotes = selectedCategory
+      ? quotes.filter((quote) => quote.category === selectedCategory)
+      : quotes;
+ 
   return (
     <>
     <GlobalStyle />
     <div className="App">
-      <Header/>
+      <Header 
+      categories={categories}
+      selectedCategory={selectedCategory}
+      handleCategoryChange={handleCategoryChange}/>
       <AppMain>
-        <QuoteDisplay />
+      <QuoteDisplay 
+      selectedCategory={selectedCategory}
+      quotes={quotes} />
+
       </AppMain>
      <Background/>
 
--- src/components/QuoteDisplay.js
+++ src/components/QuoteDisplay.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import quotes from '../data/quotes.json';
+import React, { useState, useEffect, useCallback } from 'react';
+//import quotes from '../data/quotes.json';
 import styled from 'styled-components';
-
-
 
 const QuoteDisplayContainer = styled.div`
 display: flex;
@@ -23,16 +21,14 @@ background-color: #ffffff;
 
 const QuoteText = styled.p`
 font-family: Arial, sans-serif;
-  
-font-size: 1 rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-  color: #333;
-  text-align: center;
-  max-width: 80%;
-  height: 20vh;
+font-size: 1rem;
+font-weight: bold;
+margin-bottom: 1rem;
+color: #333;
+text-align: center;
+max-width: 80%;
+height: 20vh;
 `;
-
 
 const QuoteYear = styled.p`
   font-size: 24px;
@@ -44,9 +40,7 @@ const QuotePerson = styled.p`
   font-size: 30px;
   margin-bottom: 16px;
   font-style: italic;
-
 `;
-
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -64,7 +58,6 @@ const Button = styled.button`
   cursor: pointer;
   font-size: 1.5rem;
   padding: 1rem;
-  cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
@@ -80,19 +73,29 @@ const Button = styled.button`
   }
 `;
 
-function QuoteDisplay() {
+function QuoteDisplay({selectedCategory, quotes}) {
+    
   const [quote, setQuote] = useState('');
 
-
-  const getRandomQuote = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+  const getRandomQuote = useCallback(() => {
+        // Filter quotes by category if a category is selected
+        const filteredQuotes = selectedCategory
+        ? quotes.filter((quote) => quote.category === selectedCategory)
+        : quotes;
+    
+if (!filteredQuotes || filteredQuotes.length === 0) {
+      console.error('No quotes data available');
+      return;
+    }
+    
+    const randomIndex = Math.floor(Math.random() * filteredQuotes.length);
+    const randomQuote = filteredQuotes[randomIndex];
     setQuote(`${randomQuote.year} - ${randomQuote.person} - ${randomQuote.description} - ${randomQuote.source}`);
-  };
+  }, [selectedCategory, quotes]);
 
   useEffect(() => {
     getRandomQuote();
-  }, []);
+  }, [getRandomQuote]);
 
   if (typeof quote !== 'string') {
     return null;
@@ -100,21 +103,16 @@ function QuoteDisplay() {
 
   const [year, person, description, source] = quote.split(' - ');
 
-
   return (
-    
- 
    <QuoteDisplayContainer>
       <QuoteYear>{year}</QuoteYear>
       <QuotePerson>{person}</QuotePerson>
-      <QuoteText> {description}</QuoteText>
+      <QuoteText>{description}</QuoteText>
       <ButtonContainer>
-
       <Button onClick={getRandomQuote}>New Quote</Button><br/>
       <Button onClick={() => window.open(source, '_blank')}>Learn More</Button>
       </ButtonContainer>
-      </QuoteDisplayContainer>
-   
+    </QuoteDisplayContainer>
   );
 }
 
